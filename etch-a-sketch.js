a7e1d76d72a7a970d230a1219f5aee95f35ca5f6
yamne-it <61476433+yamne-it@users.noreply.github.com>
2022-05-11 10:39:20 +0200

    Added controls to handle grid size and update style.css

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,17 +1,38 @@
-// get the container
-const container = document.getElementById("container");
+// Grid sizes
+const GRID_SMALL = 16;
+const GRID_MEDIUM = 32;
+const GRID_BIG = 64;
 
-function createGrid(rows, cols) {
+const createGrid = (rows, cols, classItem) => {
+  const container = document.getElementById("grid-container");
+  container.innerHTML = "";
   container.style.setProperty('--grid-rows', rows);
   container.style.setProperty('--grid-cols', cols);
+
   for (let i = 1; i <= (rows * cols); i++) {
     const cell = document.createElement("div");
-    cell.className = "grid-item";
-    cell.onmouseover = () => cell.classList.add("hovered");
+    cell.classList.add("grid-item");
+    cell.classList.add(classItem);
+    cell.mouseOverCount = 0;
+    cell.onmouseover = (event) => hovered(event);
     cell.onmouseout = () => cell.classList.remove("hovered");
-    cell.innerText = i;
+    // cell.innerText = i;
     container.appendChild(cell);
   };
 };
+
+const btnSizeSmall = document.getElementById("btn-size-small");
+btnSizeSmall.addEventListener('click', () => createGrid(GRID_SMALL, GRID_SMALL, "small-grid"));
+const btnSizeMedium = document.getElementById("btn-size-medium");
+btnSizeMedium.addEventListener('click', () => createGrid(GRID_MEDIUM, GRID_MEDIUM, "medium-grid"));
+const btnSizeBig = document.getElementById("btn-size-big");
+btnSizeBig.addEventListener('click', () => createGrid(GRID_BIG, GRID_BIG, "big-grid"));
+
  
-createGrid(16, 16);
+const hovered = (event) => {
+  const item = event.target;
+  item.mouseOverCount < 10 ? item.mouseOverCount++ : item.mouseOverCount;
+  item.style.background = `rgba(0, 0, 0, ${0.1 * item.mouseOverCount})`;
+}
+
+createGrid(GRID_SMALL, GRID_SMALL, "small-grid");
